3d5f5aef361e31b268d91a1707f86f384cc50906
thekoc11 <thekoc11@gmail.com>
2018-10-23 12:31:04 +0530

    MonteCarlo 23/10/18

---
+++ MonteCarlo/particle.js
@@ -0,0 +1,320 @@
+var Particle = {
+  index: -Infinity,
+  radius: 10,
+  mass: 2,
+  x: 0,
+  y: 0,
+  z: 0,
+  vx: 0,
+  vy: 0,
+  vz: 0,
+  ax: 0,
+  ay: 0,
+  az: 0,
+  KE: 0,
+  xp: 0,
+  yp: 0,
+  zp: 0,
+  vxp: 0,
+  vyp: 0,
+  vzp: 0,
+  axp: 0,
+  ayp: 0,
+  azp: 0,
+  _x: 0,
+  _y: 0,
+  _z: 0,
+  potential: 0,
+  isForceCalculated: [[]], 
+
+  create: function(x, y, z, i){
+    var obj = Object.create(this);
+    obj.setX(x);
+    obj.setY(y);
+    obj.setZ(z);
+    obj.setVx(-10, 10);
+    obj.setVy(-10, 10);
+    obj.setVz(-10, 10);
+    obj.KE = obj.vx**2 + obj.vy**2 + obj.vz**2;
+    obj.xp = -Infinity; obj.yp = -Infinity; obj.zp = -Infinity;
+    obj.index = i;
+    
+    for(var i = 0; i < 100; i++){
+      obj.isForceCalculated.push([]);
+      for(var j = 0; j < 100; j++){
+        obj.isForceCalculated[i].push(false);
+      }
+    }
+    return obj;
+  },
+
+  setX: function(x){
+    this.x = x;
+  },
+  setY: function(y){
+    this.y = y;
+  },
+  setZ: function(z){
+    this.z = z;
+  },
+  setVx : function(lb, ub){
+    this.vx = Math.random() * (ub - lb) + lb;
+  },
+  setVy : function(lb, ub){
+    this.vy = Math.random() * (ub - lb) + lb;
+  },
+  setVz : function(lb, ub){
+    this.vz = Math.random() * (ub - lb) + lb;
+  },
+  getX: function(){
+    return this.x;
+  },
+  getY: function(){
+    return this.y;
+  },
+  getZ: function(){
+    return this.z;
+  },
+
+  getKE: function(){
+    return this.KE
+  },
+
+  getVx: function(){
+    return this.vx;
+  },
+  getVy: function(){
+    return this.vy;
+  },
+  getVz: function(){
+    return this.vz;
+  },
+
+  getVelocity: function(){
+    return Math.sqrt(this.vx ** 2 + this.vy ** 2 + this.vz ** 2);
+  },
+
+  getVelocitySquare: function(){
+    return (this.vx ** 2 + this.vy ** 2 + this.vz ** 2);
+  },
+
+  distanceTo: function(p2, boxLength){//follows nearest image convenction
+    var X = p2.getX(), Y = p2.getY(), Z = p2.getZ();
+    var x = this.x, y = this.y, z = this.z;
+    var _x = X - x, _y = Y - y, _z = Z - z;
+    _x = _x - boxLength * Math.floor(_x/boxLength);
+    _y = _y - boxLength * Math.floor(_y/boxLength);
+    _z = _z - boxLength * Math.floor(_z/boxLength);	
+    var dist = Math.sqrt(_x**2 + _y**2 + _z**2);
+    // console.log("distance between particles", _x, _y, _z);
+
+    this._x = _x; this._y = _y; this._z = _z;
+    return dist;
+  },
+  
+  updateVerlet: function(t, boxLength){
+    var xtemp = this.x, ytemp = this.y, ztemp = this.z;
+    // console.log("Current positions before verlet update are ", this.x, this.y, this.z);
+
+    this.setX(2*this.x - this.xp + this.ax*t**2);
+    this.setY(2*this.y - this.yp + this.ay*t**2);
+    this.setZ(2*this.z - this.zp + this.az*t**2);
+    // console.log("Current positions are ", this.x, this.y, this.z);
+
+    this.vx = (this.x - this.xp)/2*t;
+    this.vy = (this.y - this.yp)/2*t;
+    this.vz = (this.z - this.zp)/2*t;
+
+    this.xp = xtemp;
+    this.yp = ytemp;
+    this.zp = ztemp;
+    // console.log("Current velocities are ", this.vx, this.vy, this.vz);
+
+    this.boundaryCheck(boxLength);
+
+  },
+
+  updateVelocityVerlet: function(t, boxLength){
+    var xtemp = this.x, ytemp = this.y, ztemp = this.z;
+    var vxtemp = this.vx, vytemp = this.vy, vztemp = this.vz;
+    var axtemp = this.ax, aytemp = this.ay, aztemp = this.az;
+    this.setX(this.xp + this.vxp*t + 0.5*this.axp*t**2);
+    this.setY(this.yp + this.vyp*t + 0.5*this.ayp*t**2);
+    this.setZ(this.zp + this.vzp*t + 0.5*this.azp*t**2);
+
+    this.vx = this.vxp + 0.5*(this.axp + this.ax)*t;
+    this.vy = this.vyp + 0.5*(this.ayp + this.ay)*t;
+    this.vz = this.vzp + 0.5*(this.azp + this.az)*t;
+
+    this.xp = this.x;
+    this.yp = this.y;
+    this.zp = this.z;
+    this.vxp = this.vx;
+    this.vyp = this.vy;
+    this.vzp = this.vz;
+    this.axp = this.ax;
+    this.ayp = this.ay;
+    this.azp = this.az;    
+
+    this.boundaryCheck(boxLength);
+
+  },
+
+  boundaryCheck: function(boxLength){//Periodic Boundary Condition
+    if(this.x < -boxLength/2)
+      this.x = this.x + boxLength;
+    else if (this.x > boxLength/2)
+      this.x = this.x - boxLength;
+    if(this.y < -boxLength/2)
+      this.y = this.y + boxLength;
+    else if (this.y > boxLength/2)
+      this.y = this.y - boxLength;
+    if(this.z < -boxLength/2)
+      this.z = this.z + boxLength;
+    else if (this.z > boxLength/2)
+      this.z = this.z - boxLength;
+    
+  },
+  normaliseVelocity: function(avgVelocity){
+    this.vx = this.vx - avgVelocity;
+    this.vy = this.vy - avgVelocity;
+    this.vz = this.vz - avgVelocity;
+  },
+  rescaleVelocity: function(scalingFactor){
+    this.vx = this.vx * scalingFactor;
+    this.vy = this.vy * scalingFactor;
+    this.vz = this.vz * scalingFactor;
+  },
+  calcPreviousPosition: function(DeltaT, boxLength){
+    // console.log("Current positions are ", this.x, this.y, this.z);
+
+    this.xp = this.x - this.vx * DeltaT;
+    this.yp = this.y - this.vy * DeltaT;
+    this.zp = this.z - this.vz * DeltaT;
+    if(this.xp < -boxLength/2)
+      this.xp = this.xp + boxLength;
+    else if (this.xp > boxLength/2)
+      this.xp = this.xp - boxLength;
+    if(this.yp < -boxLength/2)
+      this.yp = this.yp + boxLength;
+    else if (this.yp > boxLength/2)
+      this.yp = this.yp - boxLength;
+    if(this.zp < -boxLength/2)
+      this.zp = this.zp + boxLength;
+    else if (this.zp > boxLength/2)
+      this.zp = this.zp - boxLength;
+
+      // console.log("Current previous positions are ", this.xp, this.yp, this.zp, boxLength/2);
+
+
+  },
+
+  calcForce: function(p2, sigma, epsilon, boxLength){
+    var dist = 0;
+    if(p2.index == this.index)
+     { dist = Infinity;}
+   else{
+    var X = p2.getX(), Y = p2.getY(), Z = p2.getZ();
+    var x = this.x, y = this.y, z = this.z;
+    var _x = X - x, _y = Y - y, _z = Z - z;
+    _x = _x - boxLength * Math.floor(_x/boxLength);
+    _y = _y - boxLength * Math.floor(_y/boxLength);
+    _z = _z - boxLength * Math.floor(_z/boxLength); 
+    dist = Math.sqrt(_x**2 + _y**2 + _z**2);
+    this._x = _x; this._y = _y; this._z = _z;
+
+   }
+    
+    
+    var r = 0;
+    r = dist;
+    var ljcut = (2**(1/6))*sigma;
+    var xforce = 0, yforce = 0, zforce = 0;
+    if (r < sigma){r = sigma;}
+
+    if (r < ljcut && this.isForceCalculated[this.index][p2.index] == false) {
+       xforce =  (48 * epsilon * this._x) * ((sigma / r) ** 12 - 0.5*(sigma/r)**6)/(r**2);
+       //(48*eps*xr(i,j)*((sig/r(i,j))**12 - 0.5*(sig/r(i,j))**6))/(r(i,j)**2)
+       yforce =  (48 * epsilon * this._y) * ((sigma / r) ** 12 - 0.5*(sigma/r)**6)/(r**2);
+       zforce =  (48 * epsilon * this._z) * ((sigma / r) ** 12 - 0.5*(sigma/r)**6)/(r**2);
+      // console.log("updated accelaration is now", xforce);
+       this.isForceCalculated[this.index][p2.index] = true;
+       this.isForceCalculated[p2.index][this.index] = true;
+       p2.isForceCalculated[this.index][p2.index] = true;
+       p2.isForceCalculated[p2.index][this.index] = true;
+    // console.log("dist annd ljcut", this.isForceCalculated[this.index][p2.index], this.index, p2.index);
+
+    }
+    else{
+      xforce = 0;
+      yforce = 0;
+      zforce = 0;
+    }
+    this.ax = this.ax + xforce;
+       p2.ax = p2.ax - xforce;
+       this.ay = this.ay + yforce;
+       p2.ay = p2.ay - yforce;
+       this.az = this.az + zforce;
+       p2.az = p2.az - zforce;  
+
+       // 
+    // console.log("Current accelarations for ", this.index, p2.index, " are ", xforce, yforce, zforce, r);
+
+  },
+  
+  calcPotential: function(p2, sigma, epsilon, boxLength){
+     var dist = 0;
+     if(p2.index == this.index)
+       { dist = Infinity;}
+     else{
+       var X = p2.getX(), Y = p2.getY(), Z = p2.getZ();
+       var x = this.x, y = this.y, z = this.z;
+       var _x = X - x, _y = Y - y, _z = Z - z;
+       _x = _x - boxLength * Math.floor(_x/boxLength);
+       _y = _y - boxLength * Math.floor(_y/boxLength);
+       _z = _z - boxLength * Math.floor(_z/boxLength); 
+       dist = Math.sqrt(_x**2 + _y**2 + _z**2);
+       this._x = _x; this._y = _y; this._z = _z;
+
+     }
+    
+    
+     var r = 0;
+     r = dist;
+     var ljcut = (2**(1/6))*sigma;
+     var xforce = 0, yforce = 0, zforce = 0;
+     if (r < sigma){r = sigma;}
+
+     if (r < ljcut && this.isForceCalculated[this.index][p2.index] == false) {
+       xforce =  (4 * epsilon) * ((sigma / r) ** 12 - (sigma/r)**6);
+       //(4*eps*((sig/r(i,j))**12 - (sig/r(i,j))**6))
+       //yforce =  (48 * epsilon * this._y) * ((sigma / r) ** 12 - 0.5*(sigma/r)**6)/(r**2);
+       //zforce =  (48 * epsilon * this._z) * ((sigma / r) ** 12 - 0.5*(sigma/r)**6)/(r**2);
+      // console.log("updated accelaration is now", xforce);
+       this.isForceCalculated[this.index][p2.index] = true;
+       this.isForceCalculated[p2.index][this.index] = true;
+       p2.isForceCalculated[this.index][p2.index] = true;
+       p2.isForceCalculated[p2.index][this.index] = true;
+    // console.log("dist annd ljcut", this.isForceCalculated[this.index][p2.index], this.index, p2.index);
+
+    }
+    else{
+      xforce = 0;
+      yforce = 0;
+      zforce = 0;
+      this.isForceCalculated[this.index][p2.index] = false;
+      this.isForceCalculated[p2.index][this.index] = false;
+      p2.isForceCalculated[this.index][p2.index] = false;
+      p2.isForceCalculated[p2.index][this.index] = false;
+    }
+   this.potential = this.potential + xforce;
+   p2.potential = p2.potential + xforce;
+        
+  },
+
+  display: function(){
+    console.log("Particle position is :", this.x, this.y, this.z);
+    console.log("Particle velocity is :", this.vx, this.vy, this.vz);
+    console.log("Particle acceleration is :", this.ax, this.ay, this.az);
+  }
+};
---
+++ MonteCarlo/sketch.js
@@ -0,0 +1,226 @@
+
+
+var b;
+
+var time = [], t = 0, timeVV = [];
+var Fx = [], FxVV = [], Fy = [], FyVV = [], Fz = [], FzVV = [];
+var Px = [], Py = [], Pz = []; var PxVV = [], PyVV = [], PzVV = [];
+var T = []; var TVV = [];
+
+function setup(){
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  b = Box.create(200, 50, 0.3, 0.1);//boxlength, noOfParticles, initialTemp, dt
+  b.calcTotLJPotential();
+  bxp = Box.create(200, 50, 0.3, 0.1);//boxlength, noOfParticles, initialTemp, dt
+  bxp.calcTotLJPotential();
+
+  var DeltaV = bxp.TotalPot - b.TotalPot;
+  if(DeltaV < 0){
+    b.acceptance(bxp);
+  }
+  else if(Math.exp(DeltaV) < Math.random()){
+    b.acceptance(bxp);
+  }
+
+  b.calcTotLJPotential();
+  // b.updateVelocityVerlet();
+  // while(t<300){
+  //     time.push(t);
+  // b.updateVerlet();
+  // b.calcTotalForce();
+  // Fx.push(b.TotalFx);
+  // // console.log("Total FX is", b.TotalFx);
+  // Fy.push(b.TotalFy);
+  // Fz.push(b.TotalFz);
+  // b.calcTotalMomentum();
+  // Px.push(b.TotalMomentumX);
+  // Py.push(b.TotalMomentumY);
+  // Pz.push(b.TotalMomentumZ);
+  // b.calcTemperature();
+  // T.push(b.temperature);
+  // t++;
+  // }
+//   t = 0;
+// while(t<100){
+//   timeVV.push(t);
+//   b.updateVelocityVerlet();
+//   b.calcTotalForce();
+//   FxVV.push(b.TotalFx);
+//   FyVV.push(b.TotalFy);
+//   FzVV.push(b.TotalFz);
+//   b.calcTotalMomentum();
+//   PxVV.push(b.TotalMomentumX);
+//   PyVV.push(b.TotalMomentumY);
+//   PzVV.push(b.TotalMomentumZ);
+//   b.calcTemperature();
+//   TVV.push(b.temperature);
+//   t++;
+//   }
+//  plot();
+}
+
+function draw(){
+  background(0);
+  //lights();
+  noFill();
+  stroke(255);
+  // pushMatrix();
+  // noStroke();
+  box(b.size);//a cube of side 200
+  // popMatrix();
+
+
+  noStroke();
+  fill(255);
+
+  for(var i = 0; i < b.p.length; i+= 1){
+    push();//reset the pointer, bring it back to origin
+    translate(b.p[i].x, b.p[i].y, b.p[i].z);
+    sphere(b.p[i].radius);
+    pop();
+  }
+  /*time.push(t);
+  b.updateVerlet();
+  b.calcTotalForce();
+  Fx.push(b.TotalFx);
+  // console.log("Total FX is", b.TotalFx);
+  Fy.push(b.TotalFy);
+  Fz.push(b.TotalFz);
+  b.calcTotalMomentum();
+  Px.push(b.TotalMomentumX);
+  Py.push(b.TotalMomentumY);
+  Pz.push(b.TotalMomentumZ);
+  b.calcTemperature();
+  T.push(b.temperature);
+  t++;*/
+
+  //plot();
+  //requestAnimationFrame(draw);
+}
+
+function plot() {
+  var data1 = {
+    x: time,
+    y: Fx,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Total Force in X'
+  };
+  var data2 = {
+    x: time,
+    y: Fy,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Total Force in Y'
+  };
+  var data3 = {
+    x: time,
+    y: Fz,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Total Force in Z'
+  };
+
+  var mdata1 = {
+    x: time,
+    y: Px,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Total momentum in X'
+  };
+  var mdata2 = {
+    x: time,
+    y: Py,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Total momentum in Y'
+  };
+  var mdata3 = {
+    x: time,
+    y: Pz,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Total momentum in Z'
+  };
+  var tdata1 = {
+    x: time,
+    y: T,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Temperature'
+  };
+
+  var data = [data1, data2, data3];
+  var mdata = [mdata1, mdata2, mdata3];
+  var tdata = [tdata1];
+  var layout = {};
+
+  Plotly.newPlot('myDiv1', data, layout);
+  Plotly.newPlot('myDiv2', mdata, layout);
+  Plotly.newPlot('myDiv3', tdata, layout);
+
+  var VVdata1 = {
+    x: timeVV,
+    y: FxVV,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Velocity Verlet Total Force in X'
+  };
+
+  var VVdata2 = {
+    x: timeVV,
+    y: FyVV,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Velocity Verlet Total Force in Y'
+  };
+  var VVdata3 = {
+    x: timeVV,
+    y: FzVV,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Velocity Verlet Total Force in Z'
+  };
+
+var VVdata = [VVdata1, VVdata2, VVdata3];
+
+  var VVmdata1 = {
+    x: timeVV,
+    y: PxVV,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Velocity Verlet Total Momentum in X'
+  };
+  var VVmdata2 = {
+    x: timeVV,
+    y: PyVV,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Velocity Verlet Total Momentum in Y'
+  };
+  var VVmdata3 = {
+    x: timeVV,
+    y: PzVV,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Velocity Verlet Total Momentum in Z'
+  };
+
+  var VVmdata = [VVmdata1, VVmdata2, VVmdata3];
+
+  var VVtdata1 = {
+    x: timeVV,
+    y: TVV,
+    type: 'scatter',
+    mode: 'lines',
+    name: 'Velocity Verlet Temperature'
+  };  
+
+  var VVtdata = [VVtdata1];
+
+  Plotly.newPlot('myDivP1', VVdata, layout);
+  Plotly.newPlot('myDivP2', VVmdata, layout);
+  Plotly.newPlot('myDivP3', VVtdata, layout);
+
+}
+
